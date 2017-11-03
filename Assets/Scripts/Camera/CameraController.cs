af0ea30e4bddf84c0b1446fcc9c116aa5b0e2225
andra9612 <andra9612@ukr.net>
2017-11-03 22:30:23 +0200

    Initialize project . Added few prefbs.  Writed script  thaths  generate location.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	Camera camera;
+
+	Vector2 deltaMoving;
+
+	public GameObject dot;
+
+	Vector2 size;
+	int countOfSquares = 0;
+
+	public float moveSpeed = 10f;
+
+	float maxY = 0, maxX = 0;
+
+	// Use this for initialization
+	void Start () {
+		camera = Camera.main;
+		size = GameObject.Find ("AllFields").GetComponent<LevelGenerator>().square.GetComponent<SpriteRenderer>().size;
+		countOfSquares = GameObject.Find ("AllFields").GetComponent<LevelGenerator> ().countOfSqueres;
+
+		maxX = size.x * countOfSquares;
+		maxY = size.y * countOfSquares;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.mousePosition.y > Screen.height-10  && camera.transform.position.y  < maxY) {
+			camera.transform.Translate (0, moveSpeed  * Time.deltaTime, 0 );
+
+		}
+		if (Input.mousePosition.y < 10 && camera.transform.position.y > 0) {
+			camera.transform.Translate (0, -moveSpeed  * Time.deltaTime, 0 );
+		}
+
+		if (Input.mousePosition.x > Screen.width-10  && camera.transform.position.x  < maxX ) {
+			camera.transform.Translate (  moveSpeed  * Time.deltaTime,0, 0 );
+		}
+		if (Input.mousePosition.x <10 && camera.transform.position.x  > 0 ) {
+			camera.transform.Translate (  -moveSpeed  * Time.deltaTime,0, 0 );
+		}
+
+		if (Input.GetMouseButtonUp(0)) {
+			
+			Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+
+			RaycastHit hit = new RaycastHit ();
+
+			if (Physics.Raycast(ray, out hit)) {
+				Debug.Log ("hit");
+
+				if (hit.collider.tag.Contains("Dot")) {
+					Instantiate (dot, hit.transform.position, Quaternion.identity, hit.transform);
+					Debug.Log ("dot");
+				}
+			}
+		}
+	
+	}
+}
---
+++ Assets/Scripts/LevelGeneration/LevelGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public GameObject square;
+	public Transform parent;
+
+	public int countOfSqueres = 10;
+
+	private Vector2 size;
+
+	void Start () {
+		size = square.GetComponent<SpriteRenderer> ().size;
+
+		GenerateField ();
+	}
+
+	void GenerateField(){
+
+		GameObject variableSquare;
+
+		Vector3 deltaPosition = new Vector3 (size.x, size.y, 0);
+
+		for (int i = 0; i <= countOfSqueres; i++) {
+			for (int j = 0; j <= countOfSqueres; j++) {
+				deltaPosition.Set (size.x *j, size.y * i, 0);
+				variableSquare = Instantiate (square, deltaPosition, Quaternion.identity, parent );
+
+				if (i == countOfSqueres)
+					variableSquare.transform.GetChild (0).gameObject.SetActive (true);
+			}
+
+		}
+
+	}
+	
+
+}
